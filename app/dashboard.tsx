bcb0b79ce1ca921243e3cb6f7c2c25874e2d548a
ESSY555 <adewumiisrael66@gmail.com>
2025-11-03 14:03:03 +0100

    new commit

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -47,6 +47,7 @@ export default function Dashboard() {
         <>
             <StatusBar style="dark" backgroundColor="#FFFFFF" />
             <ScrollView
+                //this is the clickup change
                 key={`refresh-${refreshTick}`}
                 style={tw`h-full bg-[#F7F7F7]`}
                 showsVerticalScrollIndicator={false}
